b03ca033e5f124feb6eb0583131b76e3b4f4facf
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2023-03-24 20:02:31 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#Installs puppet-lint
-
-package { 'puppet-lint':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'flask',
+  provider => 'pip3'
 }
