c56bf3cbe3d3e702baf7a305af790c223c9d70bd
Ojekunle Phillip <111189447+sonofmercy123@users.noreply.github.com>
2023-03-24 19:29:01 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,20 @@
 # Install a package using Puppet
 
-exec { 'install ruby':
-  command => '/usr/bin/apt-get install -y ruby'
+exec { 'install flask':
+  command => '/usr/bin/apt-get install -y flask'
 }
 
 exec { 'install puppet-lint':
   command => '/usr/bin/gem install puppet-lint -v 2.1.1'
 }
 
-package { 'ruby':
+package { 'flask':
   ensure => 'installed',
-  before => Exec['install ruby']
+  before => Exec['install flask']
 }
 
 package { 'puppet-lint':
   ensure  => 'installed',
   before  => Exec['install puppet-lint'],
-  require => Package['ruby']
+  require => Package['flask']
 }
